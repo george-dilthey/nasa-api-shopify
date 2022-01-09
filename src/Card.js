a9b4ac5f7d84e15c996d6a49b54c6962242c12a3
George Dilthey <george.dilthey@gmail.com>
2022-01-09 17:17:19 -0500

    added like functionality

--- src/Card.js
+++ src/Card.js
@@ -5,16 +5,25 @@ import { useState } from 'react';
 
 const Card = ({image, copyright, date, explanation, title}) => {
   
+  const [liked, setLiked] = useState(false)
+
+  const handleClick = () => {
+    setLiked(!liked)
+  }
+
+
 
   useEffect(() => {
 
     }, []);
 
   return (
-    <div className='card'>
+    <div className='card' onClick={handleClick}>
       <img className='image' src={image} />
       <div className='description'>
         <div className='text'>This is my description</div>
+        <div>{liked ? 'Liked' : 'Not Liked'}</div>
+
       </div>
     </div>
   )
